48912aedaa39a4b0981e0ce97798cbdd1b635d5e
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2024-09-30 23:16:25 +0600

    30th september

--- dsa/algorithm/graph/2.py
+++ dsa/algorithm/graph/2.py
@@ -1,3 +1,4 @@
+#user input
 def buildgraph(edges):
     graph = {}
     for edge in edges: 
@@ -10,12 +11,15 @@ def buildgraph(edges):
         graph[pointB].append(pointA)
     return graph
     
-edges = [
-    ['i','j'],
-    ['k','i'],
-    ['m','k'],
-    ['k','l'],
-    ['o','n']
-]
-
+# edges = [
+#     ['i','j'],
+#     ['k','i'],
+#     ['m','k'],
+#     ['k','l'],
+#     ['o','n']
+# ]
+n = 5
+edges = []
+for i in  range(n):
+    edges.append(input().split())
 print(buildgraph(edges))
