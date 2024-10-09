d6201e3e2388121afedfd61d12280406a7112995
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2024-10-09 22:15:05 +0600

    9th october

--- dsa/algorithm/tree/coding_cart_2.py
+++ dsa/algorithm/tree/coding_cart_2.py
@@ -3,7 +3,6 @@ class Node:
         self.left = None
         self.data = value
         self.right = None
-
 class Tree:
     def createNode(self,data):
         return Node(data)
@@ -15,12 +14,23 @@ class Tree:
         else:
              node.right = self.insert(node.right,data)
         return node
-
+    def traverse_Inorder(self,root):
+        if root is not None:
+            self.traverse_Inorder(root.left)
+            print(root.data)
+            self.traverse_Inorder(root.right)
 tree = Tree()
 root = tree.createNode(5)
 print(root.data)
 print(root.left)
-
-
 tree.insert(root,2)
 tree.insert(root,10)
+tree.insert(root,7)
+tree.insert(root,15)
+tree.insert(root,12)
+tree.insert(root,20)
+tree.insert(root,30)
+tree.insert(root,6)
+tree.insert(root,8)
+print("Inorder Travers")
+tree.traverse_Inorder(root)
